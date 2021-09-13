f4bf08e905ee987b62c0411c7a63abd8f354cd6c
KAVYA123PREETHA <87599003+KAVYA123PREETHA@users.noreply.github.com>
2021-09-13 12:16:40 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,47 +1,60 @@
 import os
+pip install adafruit-io
+
+
+pip install python-telegram-bot==13.0 --quiet
+
+
+from Adafruit_IO import Client
+aio = Client(os.getenv('uname'),os.getenv('feed'))
+
+
 from telegram.ext import Updater , MessageHandler , Filters
 
 def demo1(bot,update):
   
   chat_id = bot.message.chat_id
   bot.message.reply_text('turning the lights on')
+  aio.send('light',1)
   
   
 def demo2(bot,update):
   
   chat_id = bot.message.chat_id
   bot.message.reply_text('turning the lights off')
+  aio.send('light',0)
   
   
 def demo3(bot,update):
   
   chat_id = bot.message.chat_id
   bot.message.reply_text('turning the fan on')
+  aio.send('fan',1)
   
   
 def demo4(bot,update):
   
   chat_id = bot.message.chat_id
   bot.message.reply_text('turning the fan off')
+  aio.send('fan',0)
   
   
 
 def main(bot,update):
   a = bot.message.text
-  if a=='Turn on light':
+  if a =='Turn on light':
    demo1(bot,update)
-   b=1
+   
   elif a =='Turn off light':
-    demo2(bot,update)
-    b=0
+   demo2(bot,update)
+   
   elif a =='Turn on fan':
-    demo3(bot,update)
-    b=1
+   demo3(bot,update)
+   
   elif a =='Turn off fan':
-    demo4(bot,update)
-    b=0
+   demo4(bot,update)
 
-bot_token = os.getenv('bot_token)
+bot_token = os.getenv('bot_token’)
 u = Updater(bot_token,use_context=True)
 dp = u.dispatcher
 dp.add_handler(MessageHandler(Filters.text,main))
